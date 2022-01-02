ac54ea3e156eb3551c01c65141e85d19f78f217f
lucascarlos1661 <lucascarlos1661@gmail.com>
2022-01-02 14:10:46 -0300

    Ajustando adapter e adicionando a recycler view na main activity

--- app/src/main/java/com/lucascarlos/cineticket/MainActivity.kt
+++ app/src/main/java/com/lucascarlos/cineticket/MainActivity.kt
@@ -2,10 +2,42 @@ package com.lucascarlos.cineticket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.lucascarlos.cineticket.api.MyRetrofit
+import com.lucascarlos.cineticket.model.Movies
+import com.lucascarlos.cineticket.model.MoviesAdapter
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var recycleMovies: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recycleMovies = findViewById(R.id.recycleMovies)
+        recycleMovies.layoutManager = LinearLayoutManager(this)
+        getData()
+    }
+
+    private fun getData() {
+        val call: Call<List<Movies>> =
+            MyRetrofit.instance?.moviesApi()?.getMoviesApi() as Call<List<Movies>>
+        call.enqueue(object: Callback<List<Movies>> {
+            override fun onResponse(call: Call<List<Movies>>, response: Response<List<Movies>>) {
+                val adapter = MoviesAdapter(this@MainActivity, response.body()?.toList() as List<Movies>)
+                recycleMovies.adapter = adapter
+            }
+
+            override fun onFailure(call: Call<List<Movies>>, t: Throwable) {
+                Toast.makeText(this@MainActivity, t.message, Toast.LENGTH_LONG).show()
+            }
+
+        })
     }
 }
--- app/src/main/java/com/lucascarlos/cineticket/model/MoviesAdapter.kt
+++ app/src/main/java/com/lucascarlos/cineticket/model/MoviesAdapter.kt
@@ -1,26 +1,35 @@
 package com.lucascarlos.cineticket.model
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.lucascarlos.cineticket.R
 
-class MoviesAdapter(private val context: Context, private val moviesList: List<Movies>): RecyclerView.Adapter<MoviesViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MoviesViewHolder {
-        TODO("Not yet implemented")
-    }
+class MoviesAdapter(private val context: Context, private val moviesList: List<Movies>) :
+    RecyclerView.Adapter<MoviesViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MoviesViewHolder =
+        MoviesViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.movie_item, parent, false)
+        )
+
+    override fun getItemCount(): Int = moviesList.size
 
     override fun onBindViewHolder(holder: MoviesViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val item = moviesList[position]
+        holder.movieTitle.text = item.title
+        Glide.with(context).load(item.posterUrl).into(holder.movieBanner)
     }
-
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
-
 }
 
-class MoviesViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
+class MoviesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val movieBanner: ImageView = itemView.findViewById(R.id.movie_banner)
+    val movieTitle: TextView = itemView.findViewById(R.id.movie_title)
 }
 
