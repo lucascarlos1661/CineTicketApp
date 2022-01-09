6a04401d1c29aafc639f1ea012260bdacfe85dd7
lucascarlos1661 <lucascarlos1661@gmail.com>
2022-01-08 23:26:22 -0300

    Criando recycler view para exibir os horarios dentro da recycler view de salas

---
+++ app/src/main/java/com/lucascarlos/cineticket/model/SchedulesAdapter.kt
@@ -0,0 +1,32 @@
+package com.lucascarlos.cineticket.model
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.lucascarlos.cineticket.R
+
+class SchedulesAdapter(private val context: Context, private val schedulesList: List<String>) :
+    RecyclerView.Adapter<SchedulesViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SchedulesViewHolder =
+        SchedulesViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.schedule_item, parent, false)
+        )
+
+    override fun getItemCount(): Int = schedulesList.size
+
+    override fun onBindViewHolder(holder: SchedulesViewHolder, position: Int) {
+        val item = schedulesList[position]
+        Log.e("item", item.toString())
+        holder.schedule.text = item
+    }
+
+}
+
+class SchedulesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val schedule: TextView = itemView.findViewById(R.id.schedule)
+}
