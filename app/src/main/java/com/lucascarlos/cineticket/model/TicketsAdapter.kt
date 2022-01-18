9bb6a86daaa392bc7a95659cebc0e1ed19da7299
lucascarlos1661 <lucascarlos1661@gmail.com>
2022-01-18 20:10:28 -0300

    Ajustando layout da activity my tickets, adicionando campos que estavam faltando

--- app/src/main/java/com/lucascarlos/cineticket/model/TicketsAdapter.kt
+++ app/src/main/java/com/lucascarlos/cineticket/model/TicketsAdapter.kt
@@ -1,22 +1,14 @@
 package com.lucascarlos.cineticket.model
 
 import android.content.Context
-import android.content.Intent
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.google.android.material.button.MaterialButton
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.lucascarlos.cineticket.R
-import com.lucascarlos.cineticket.view.BuyTicket
-import com.lucascarlos.cineticket.view.MovieDetails
 
 class TicketsAdapter(
     private val context: Context,
@@ -33,12 +25,21 @@ class TicketsAdapter(
 
     override fun onBindViewHolder(holder: TicketsViewHolder, position: Int) {
         val item = ticketsList[position]
-        Log.e("teste", item.toString())
         holder.movieTitle.text = item.movieTitle
+        Glide.with(context).load(item.posterUrl).into(holder.moviePoster)
+        holder.movieDate.text = item.movieDate
+        holder.movieRoom.text = item.movieRoom
+        holder.movieTime.text = item.movieTime
+        holder.movieSeats.text = item.seats.joinToString(separator = ", ")
     }
 }
 
 class TicketsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     val movieTitle: TextView = itemView.findViewById(R.id.movie_title)
+    val moviePoster: ImageView = itemView.findViewById(R.id.movie_poster)
+    val movieDate: TextView = itemView.findViewById(R.id.movie_date)
+    val movieRoom: TextView = itemView.findViewById(R.id.movie_room)
+    val movieTime: TextView = itemView.findViewById(R.id.movie_time)
+    val movieSeats: TextView = itemView.findViewById(R.id.movie_seats)
 }
 
