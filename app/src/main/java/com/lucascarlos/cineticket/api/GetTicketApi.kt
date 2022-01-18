4230ca9be00d8e4f49e8803a8450b10fb2819ceb
lucascarlos1661 <lucascarlos1661@gmail.com>
2022-01-18 20:33:50 -0300

    Alterando GET ticket para order desc

--- app/src/main/java/com/lucascarlos/cineticket/api/GetTicketApi.kt
+++ app/src/main/java/com/lucascarlos/cineticket/api/GetTicketApi.kt
@@ -5,6 +5,6 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface GetTicketApi {
-    @GET("tickets")
+    @GET("tickets?_sort=id&_order=desc")
     fun getTicketsApi(): Call<List<Ticket>>
 }
