d2ae59de9545a2aa27729a5645b5a7aad6d9fc27
lucascarlos1661 <lucascarlos1661@gmail.com>
2022-01-20 22:34:47 -0300

    Adicionando nova tela Ticket Details

--- app/src/main/java/com/lucascarlos/cineticket/model/TicketsAdapter.kt
+++ app/src/main/java/com/lucascarlos/cineticket/model/TicketsAdapter.kt
@@ -1,14 +1,18 @@
 package com.lucascarlos.cineticket.model
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.google.gson.Gson
 import com.lucascarlos.cineticket.R
+import com.lucascarlos.cineticket.view.TicketDetails
 
 class TicketsAdapter(
     private val context: Context,
@@ -31,10 +35,30 @@ class TicketsAdapter(
         holder.movieRoom.text = item.movieRoom
         holder.movieTime.text = item.movieTime
         holder.movieSeats.text = item.seats.joinToString(separator = ", ")
+
+        holder.card.setOnClickListener {
+
+            val ticketData = Ticket(
+                movieTitle = item.movieTitle,
+                posterUrl = item.posterUrl,
+                movieRoom = item.movieRoom,
+                movieDate = item.movieDate,
+                movieTime = item.movieTime,
+                seats = item.seats
+            )
+
+            val gson = Gson()
+            val jsonTicket = gson.toJson(ticketData)
+
+            val intent = Intent(context, TicketDetails::class.java)
+            intent.putExtra("ticket", jsonTicket)
+            context.startActivity(intent)
+        }
     }
 }
 
 class TicketsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val card: CardView = itemView.findViewById(R.id.card)
     val movieTitle: TextView = itemView.findViewById(R.id.movie_title)
     val moviePoster: ImageView = itemView.findViewById(R.id.movie_poster)
     val movieDate: TextView = itemView.findViewById(R.id.movie_date)
---
+++ app/src/main/java/com/lucascarlos/cineticket/view/TicketDetails.kt
@@ -0,0 +1,56 @@
+package com.lucascarlos.cineticket.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.gson.Gson
+import com.lucascarlos.cineticket.R
+import com.lucascarlos.cineticket.model.Ticket
+import retrofit2.converter.gson.GsonConverterFactory
+
+class TicketDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ticket_details)
+
+
+        val response = intent.getStringExtra("ticket")
+        val route: String? = intent.getStringExtra("route")
+
+        if (route == "newTicket") {
+            Snackbar.make(
+                findViewById(android.R.id.content),
+                "Ingresso reservado",
+                Snackbar.LENGTH_LONG
+            )
+                .show()
+        }
+
+        val gson = Gson()
+        val ticket: Ticket = gson.fromJson(response, Ticket::class.java)
+
+        val movieTitle: TextView = findViewById(R.id.movie_title)
+        val moviePoster: ImageView = findViewById(R.id.movie_poster)
+        val movieRoom: TextView = findViewById(R.id.movie_room)
+        val movieDate: TextView = findViewById(R.id.movie_date)
+        val movieTime: TextView = findViewById(R.id.movie_time)
+        val movieSeats: TextView = findViewById(R.id.movie_seats)
+
+        movieTitle.text = ticket.movieTitle
+        Glide.with(this).load(ticket.posterUrl).into(moviePoster)
+        movieDate.text = ticket.movieDate
+        movieRoom.text = ticket.movieRoom
+        movieTime.text = ticket.movieTime
+        movieSeats.text = ticket.seats.joinToString(separator = ",")
+    }
+
+    private fun showSnackbar() {
+
+    }
+}
